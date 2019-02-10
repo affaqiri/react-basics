ff0f351e2e46927f0c63c10467c06f0f303f29c6
Ahmad Fraidoon Faqiri <affaqiri@outlook.com>
2019-02-10 17:24:53 +1100

    JSX compilation and restriction

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      // We can only return one root element in JSX
       <div className="App">
         <h1>React App</h1>
       </div>
     );
+    // The above JSX code will get compiled behind the scenes to this JS code by React
+    // return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'React App'));
   }
 }
 
