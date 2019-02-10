d274105ddc8b2e374b145255e80708372325b1f1
Ahmad Fraidoon Faqiri <affaqiri@outlook.com>
2019-02-10 18:05:39 +1100

    adding state to our component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import './App.css'
 import Person from "./Person/Person" // Custom components must be capital case as best practice
 
 class App extends Component {
+  
+  // state can only be used withing class components
+  // any state change will rerender the DOM tree to reflect the changes
+  state = {
+    persons: [
+      { name: 'Ahmad', age: 34 },
+      { name: 'Lina', age: 30 },
+      { name: 'Farhad', age: 40 }
+    ]
+  }
+  
   render() {
     return (
       // We can only return one root element in JSX
       <div className="App">
         <h1>React App</h1>
-        <Person name='Ahmad' age='34' />
-        <Person name='Lina' age='30' />
-        <Person name='Farhad' age='40'>My Hobbies: fishing</Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+          My Hobbies: fishing
+        </Person>
       </div>
     );
     // The above JSX code will get compiled behind the scenes to this JS code by React
