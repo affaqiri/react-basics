9136e9680abe9e33b88c28df6fda2f41440ba86c
Ahmad Fraidoon Faqiri <affaqiri@outlook.com>
2019-02-17 14:17:26 +1100

    Passing method references between components - without argument

--- src/App.js
+++ src/App.js
@@ -38,9 +38,16 @@ class App extends Component {
       <div className="App">
         <h1>React App</h1>
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler} />
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}>
           My Hobbies: fishing
         </Person>
       </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from "react"
 
 // props is an object passed to our component by React
+// functional (stateless) components should be used as much as possible 
+// these components do not manipulate the application state and therefore do not introduce side effects
 const person = (props) => {
   return ( // return () used for multiple line returns
     <div>
-      <p>I am { props.name } and I am { props.age } years old.</p>
+      <p onClick={ props.click }>I am { props.name } and I am { props.age } years old.</p>
       <p>{ props.children }</p>
     </div>
   )
